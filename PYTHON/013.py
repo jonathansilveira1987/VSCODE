80938e893cd058a13246526a9504ad9f30fccb30
Jonathan <jonathan.silveira@outlook.com>
2026-03-01 23:06:08 -0300

    Codando

---
+++ PYTHON/013.py
@@ -0,0 +1,8 @@
+nomes = ["Alice", "Lucas", "Charles", "Davi"]
+for i in range(len(nomes)):
+    print(i, nomes[i])
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+    print()

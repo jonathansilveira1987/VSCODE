6932d010bfff648b82bf9ad6e2a333f8ba2b91bd
Jonathan <jonathan.silveira@outlook.com>
2026-04-03 19:32:07 -0300

    Codando

--- PYTHON/concurso.py
+++ PYTHON/concurso.py
@@ -1,2 +1,9 @@
-for i in range(900 + 1):
-    print(i)
+from time import sleep
+
+a = int(input('\nDigite um número: '))
+b = int(input('Digite um número: '))
+print(f'\n{b} + {a} = {b + a}\n')
+for i in range(b + a + 1):
+    sleep(0.3)
+    print(f'{i}')
+print()
